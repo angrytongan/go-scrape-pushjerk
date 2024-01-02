c1b984e9178029aa3c0bf8dc25427552d3b4c348
Dean Fogarty <angrytongan@gmail.com>
2024-01-02 18:32:41 +1000

    Use closures instead.

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -5,37 +5,59 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-type MyHandler struct {
-	DB *sql.DB
-}
-
 func New(db *sql.DB) chi.Router {
-	m := &MyHandler{db}
 	r := chi.NewRouter()
 
 	r.Use(middleware.Logger)
-	r.Get("/", m.Home)
+	r.Get("/", Home(db))
+	r.Get("/random", Random(db))
 
 	return r
 }
 
-func (m *MyHandler) Home(w http.ResponseWriter, r *http.Request) {
-	row := m.DB.QueryRow("SELECT COUNT(id) FROM posts")
+func Home(db *sql.DB) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		row := db.QueryRow("SELECT COUNT(id) FROM posts")
 
-	var num int
-	err := row.Scan(&num)
-	if err != nil {
-		log.Printf("failed to scan post count", err)
-		http.Error(w, "failed to scan post count", http.StatusInternalServerError)
-		return
+		var num int
+		err := row.Scan(&num)
+		if err != nil {
+			log.Printf("failed to scan post count", err)
+			http.Error(w, "failed to scan post count", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-type", "text/html")
+
+		w.Write([]byte(fmt.Sprintf("home page - %d workouts", num)))
 	}
+}
 
-	w.Header().Set("Content-type", "text/html")
+type Workout struct {
+	id      string
+	title   string
+	content string
+}
 
-	w.Write([]byte(fmt.Sprintf("home page - %d workouts", num)))
+func Random(db *sql.DB) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		row := db.QueryRow("SELECT id, title, content FROM posts ORDER BY RANDOM() LIMIT 1")
+
+		var workout Workout
+		err := row.Scan(&workout.id, &workout.title, &workout.content)
+		if err != nil {
+			log.Printf("failed to scan workout", err)
+			http.Error(w, "failed to scan workout", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-type", "text/html; charset=utf-8")
+		w.Write([]byte(fmt.Sprintf("random - %s %s %s", workout.id, workout.title, strings.ReplaceAll(workout.content, "\n", "<br />"))))
+	}
 }
