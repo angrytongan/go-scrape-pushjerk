7531aa3629d98c52d441806cb22c6cf52de5e4d6
Dean Fogarty <angrytongan@gmail.com>
2025-06-24 19:12:39 +1000

    Convert pound ranges and words; print styling.

--- cmd/web/metric.go
+++ cmd/web/metric.go
@@ -61,12 +61,56 @@ func addCm(s string) string {
 	return s
 }
 
+func addKilosWords(s string) string {
+	re := regexp.MustCompile(` (\d+) pounds`)
+	matches := re.FindSubmatch([]byte(s))
+	if len(matches) == 2 {
+		kg := 0
+
+		pounds, err := strconv.Atoi(string(matches[1]))
+		if err == nil {
+			kg = poundsToKilos(pounds)
+		}
+
+		return fmt.Sprintf("%s (%d kg)", s, kg)
+	}
+
+	return s
+}
+
+func addKilosRange(s string) string {
+	re := regexp.MustCompile(`(\d+)-(\d+) pounds`)
+	matches := re.FindSubmatch([]byte(s))
+	if len(matches) == 3 {
+		aKg := 0
+		bKg := 0
+
+		aPounds, err := strconv.Atoi(string(matches[1]))
+		if err == nil {
+			aKg = poundsToKilos(aPounds)
+		}
+
+		bPounds, err := strconv.Atoi(string(matches[2]))
+		if err == nil {
+			bKg = poundsToKilos(bPounds)
+		}
+
+		return fmt.Sprintf("%s (%d-%d kg)", s, aKg, bKg)
+	}
+
+	return s
+}
+
 func Metricise(workout string) string {
 	kilos := regexp.MustCompile(`(\d+)/(\d+)#`)
 	cm := regexp.MustCompile(`(\d+)/(\d+)”`)
+	pounds := regexp.MustCompile(` (\d+) pounds`)
+	poundsRange := regexp.MustCompile(`(\d+)-(\d+) pounds`)
 
 	out := kilos.ReplaceAllStringFunc(workout, addKilos)
 	out = cm.ReplaceAllStringFunc(out, addCm)
+	out = pounds.ReplaceAllStringFunc(out, addKilosWords)
+	out = poundsRange.ReplaceAllStringFunc(out, addKilosRange)
 
 	return out
 }
