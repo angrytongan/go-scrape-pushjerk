21bbe99b01a182216481e656dac6a7a91ccbe822
Dean Fogarty <angrytongan@gmail.com>
2025-07-04 16:03:19 +1000

    WIP Try and clean this up a bit.

--- cmd/cli/normalise/normalise.go
+++ cmd/cli/normalise/normalise.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"pj/internal/metricise"
 	"regexp"
 	"strings"
 )
@@ -12,52 +13,49 @@ type TagRemoval struct {
 
 var (
 	tagCleaning = []TagRemoval{
-		{From: `\r`, To: " "}, // newlines
-		{From: `\n`, To: " "}, // newlines
-		{From: `\t`, To: " "}, // tabs
-		{From: `<p><strong>CROSSFIT GAMES WEEK</strong></p>`, To: " "},
-		{From: `<p><strong>HAPPY APRIL FOOLS DAY!</strong></p>`, To: " "},
-		{From: `<p><strong>APRIL FOOLS’ DAY!</strong></p>`, To: " "},
-		{From: `<p><strong>HAPPY NEW YEAR</strong></p>`, To: " "},
-		{From: `<p><strong>HAPPY NEW YEAR!</strong></p>`, To: " "},
-		{From: `<p><strong>MERRY CHRISTMAS!</strong></p>`, To: " "},
-		{From: `<p><strong>HAPPY VALENTINES DAY!</strong></p>`, To: " "},
-		{From: `<p><strong>HAPPY HALLOWEEN!</strong></p>`, To: " "},
-		{From: `<p><strong>HAPPY 4<sup>TH</sup> OF JULY!</strong></p>`, To: " "},
-		{From: `<p><strong>HAPPY ST. PATRICK’S DAY</strong></p>`, To: " "},
-		{From: `<p><strong>–.*OPTION.*?</strong></p>`, To: " "},
-		{From: `<p><strong><em>Rest.*?</em></strong></p>`, To: " "},
-		{From: `<p><em>Week [56]/16`, To: `<strong>Strength</strong><p><em>Week 6/16`},
-		{From: `<strong><em> </em></strong>`, To: " "},
-		{From: `</em></strong><em>21-15-9 reps:</em><br/>`, To: " "},
-		{From: `<p><strong><em>ADJUST.*?</em></strong>.*?</p>`, To: " "},
-		{From: `<strong><br/> *</strong>`, To: " "},
-		{From: `<strong>•</strong>`, To: " "}, // bold dot things
-		{From: `<strong><br/><em>4 rounds:</em><br/></strong>`, To: " "},
-		{From: `<a.*?>`, To: " "},      // anchors
-		{From: `</a>`, To: " "},        // closing anchor
-		{From: `<div.*?>`, To: " "},    // divs
-		{From: `</div>`, To: " "},      // closing div
-		{From: `<header.*?>`, To: " "}, // header tag
-		{From: `</header>`, To: " "},   // closing header tag
-		{From: `<footer.*?>`, To: " "}, // footer tag
-		{From: `</footer>`, To: " "},   // closing footer tag
-		{From: `<span.*?>`, To: " "},   // spans
-		{From: `</span>`, To: " "},     // closing spans
-		{From: `<time.*?>`, To: " "},   // time tag
-		{From: `</time>`, To: " "},     // closing time tag
-		{From: `<h2.*?>`, To: " "},     // 2nd header
-		{From: `</h2>`, To: " "},       // closing 2nd header
-		{From: `<p.*?>`, To: " "},      // paragraphs
-		{From: `</p>`, To: " "},        // closing paragraphs
-		{From: `<br *?/>`, To: " "},    // breaks
-		{From: `<em>`, To: " "},        // emphasis
-		{From: `</em>`, To: " "},       // closing emphasis
-		{From: `</strong>`, To: " "},   // ending strong tag
-		{From: `•`, To: " "},           // Uh, some UTF thing
-		{From: ` +`, To: " "},          // dedupe spaces
+		// Tags to delete.
+		{From: `\r`, To: ""}, // newlines
+		{From: `\n`, To: ""}, // newlines
+		{From: `\t`, To: ""}, // tabs
+		//{From: `<br *?/>`, To: ""},                // breaks
+		{From: `<em>`, To: " "},                   // emphasis
+		{From: `</em>`, To: " "},                  // closing emphasis
+		{From: `•`, To: " "},                      /* weird utf thing */
+		{From: `<a.*?>`, To: " "},                 // anchors
+		{From: `</a>`, To: " "},                   // closing anchor
+		{From: `<div.*?>`, To: " "},               // divs
+		{From: `</div>`, To: " "},                 // closing div
+		{From: `<header.*?>`, To: " "},            // header tag
+		{From: `</header>`, To: " "},              // closing header tag
+		{From: `<footer.*>.*?</footer>`, To: " "}, // footer tag
+		{From: `<span.*?>`, To: " "},              // spans
+		{From: `</span>`, To: " "},                // closing spans
+		{From: `<time.*?>`, To: " "},              // time tag
+		{From: `</time>`, To: " "},                // closing time tag
+		{From: `<h2.*?>`, To: " "},                // 2nd header
+		{From: `</h2>`, To: " "},                  // closing 2nd header
+		{From: `<p.*?>`, To: " "},                 // paragraphs
+		{From: `</p>`, To: " "},                   // closing paragraphs
+		{From: `<em> </em>`, To: " "},             // empty em tags
 
-		{From: "<strong> *", To: "<strong>"}, //
+		// Dedupe spaces.
+		{From: ` +`, To: " "},
+
+		/*
+			// Uncommon strings inside <strong> tags.
+			{From: `<p><strong>CROSSFIT GAMES WEEK</strong></p>`, To: ""},
+			{From: `<p><strong>HAPPY.*?</strong></p>`, To: ""},
+			{From: `<p><strong>–.*OPTION.*?</strong></p>`, To: ""},
+			{From: `<p><em>Week [56]/16`, To: `<strong>Strength</strong><p><em>Week 5/16`},
+			{From: `</em></strong><em>21-15-9 reps:</em><br/>`, To: " "},
+			{From: `<p><strong><em>ADJUST.*?</em></strong>.*?</p>`, To: " "},
+			{From: `<strong><br/> *</strong>`, To: " "},
+			{From: `<strong><br/><em>4 rounds:</em><br/></strong>`, To: " "},
+
+			// Identifiers.
+			{From: `</strong>`, To: " "},          // Delete ending strong tags.
+			{From: "<strong> *?", To: "<strong>"}, //
+		*/
 	}
 )
 
@@ -78,7 +76,11 @@ func cleanTags(src string) string {
 func normalise(src string) []WorkoutSection {
 	workoutSections := []WorkoutSection{}
 
-	cleaned := cleanTags(src)
+	cleaned := cleanTags(metricise.Metricise(src))
+
+	// sections identified with <strong>.*</strong>
+	// split at that tag
+	// then split second string at <br/> for individual movements
 
 	sections := strings.Split(cleaned, "<strong>")
 	if len(sections) == 0 {
